3819d438436e99c85e9d5a7228f0fd5a226d9fce
nathans <nathans@carallon.com>
2022-10-09 21:36:03 +0100

    Remove parameters gui elements
    This should be complete re-thought to an elegant solution

--- src/boids/gui.py
+++ src/boids/gui.py
@@ -1,5 +1,4 @@
-from tkinter import E, N, S, W, Canvas, Entry, Frame, Label, StringVar, Tk
-from turtle import width
+from tkinter import E, N, S, W, Canvas, Frame, Tk
 
 _BOID_SIZE = 10
 _INTERVAL_LENGTH = 50
@@ -15,30 +14,8 @@ class Gui:
         self._mainframe = Frame(self._root)
         self._mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
 
-        self._parameters_frame = Frame(self._mainframe)
-        self._parameters_frame.grid(column=1, row=0, sticky=(N, E, S))
-
-
-        # TODO: Populate this with parameter value
-        self._fly_to_centre_time = StringVar(value=1)
-        self._fly_to_centre_time_entry = Entry(self._parameters_frame, textvariable=self._fly_to_centre_time)
-        self._fly_to_centre_time_entry.grid(column=1, row=0)
-
-        self._fly_to_centre_time_name = StringVar(value="Fly to centre time")
-        self._fly_to_centre_time_label = Label(self._parameters_frame, textvariable=self._fly_to_centre_time_name)
-        self._fly_to_centre_time_label.grid(column=0, row=0)
-
-
-        #self._fly_to_centre_time_entry.pack()
-        #self._fly_to_centre_time_label.pack()
-
-        #self._parameters_frame.pack()
-        
-        #self._root.pack()
-
         self._canvas = Canvas(self._mainframe, width=canvas_size, height=canvas_size)
         self._canvas.grid(sticky=(W, N, S))
-        #self._canvas.pack()
             
     def start(self):
         self._root.mainloop()
